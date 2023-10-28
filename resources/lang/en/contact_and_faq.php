19fa4daef504328e30b27c75abf41005699b0619
ezegyfa2 <darvasvok@gmail.com>
2023-10-28 07:47:08 +0300

    Fixing

--- resources/lang/en/contact_and_faq.php
+++ resources/lang/en/contact_and_faq.php
@@ -23,7 +23,7 @@ return [
         'subtitle' => 'Here are the most frequently asked questions',
         'question1' => 'What is the essence of the two-wheeled vehicle developed by EdiWheel?',
         'question2' => 'What is the performance and range of the EdiWheel two-wheeled vehicle?',
-        'question3' => 'What is the target group for EdiWheel&amp;aps;s two-wheeled vehicles?',
+        'question3' => 'What is the target group for EdiWheel&amp;apos;s two-wheeled vehicles?',
         'question4' => 'What are the environmental characteristics of the EdiWheel vehicle?',
         'question5' => 'What safety features do EdiWheel vehicles offer?',
         'question6' => 'What plans does EdiWheel have for the future?',
@@ -31,7 +31,7 @@ return [
         'answer2' => 'The performance and range of EdiWheel vehicles varies between models. In general, however, their electric motors offer good performance and sufficient range for urban transport.',
         'answer3' => 'EdiWheel two-wheelers can be useful for a variety of target groups, including city dwellers, students, workers and tourists looking for light and environmentally friendly transport.',
         'answer4' => 'EdiWheel vehicles are electrically powered, which means zero carbon emissions. The startup can also use sustainable materials to build them.',
-        'answer5' => 'EdiWheel&amp;aps;s vehicles have safety features such as braking systems, lighting and a stable design to allow users to drive them safely.',
+        'answer5' => 'EdiWheel&amp;apos;s vehicles have safety features such as braking systems, lighting and a stable design to allow users to drive them safely.',
         'answer6' => 'EdiWheel will continue to develop and expand its range of products and expand its market to make its two-wheeled vehicles available in more cities and countries. They also focus on innovation and sustainability in the development of their products',
     ],
 ];
